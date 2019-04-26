ba6bca5d97370d3f294485afbdcf916cdf9feec5
Afiq Hamdan <afiq@Afiqs-MacBook-Pro.local>
2019-04-26 11:16:32 +0800

    First Commit

---
+++ AFPickFlavor/AFPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  AFPickFlavor.h
+//  AFPickFlavor
+//
+//  Created by Afiq Hamdan on 26/04/2019.
+//  Copyright © 2019 amtrust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFPickFlavor.
+FOUNDATION_EXPORT double AFPickFlavorVersionNumber;
+
+//! Project version string for AFPickFlavor.
+FOUNDATION_EXPORT const unsigned char AFPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFPickFlavor/PublicHeader.h>
+
+
